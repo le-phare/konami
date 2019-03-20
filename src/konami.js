75335a5f59d7e97fad5c72ee6cfc7ee66c0deb8e
Nicolas LAURENT <innercircle@aegypius.com>
2019-03-20 17:54:41 +0100

    Update dependencies

--- src/konami.js
+++ src/konami.js
@@ -1,7 +1,7 @@
 const kc = (c) => {
   const seq = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   let [...q] = seq;
-  document.addEventListener('keyup', (e) => {
+  document.addEventListener('keyup', e => {
     if (q[0] === e.keyCode) {
       q.shift();
       if (!q.length) {
