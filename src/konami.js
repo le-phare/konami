28ee35b8d7504e0572ab30015bdef2bb2907f27b
Nicolas LAURENT <innercircle@aegypius.com>
2021-03-09 15:56:53 +0100

    chore: update dependencies

--- src/konami.js
+++ src/konami.js
@@ -1,17 +1,17 @@
 const kc = (c) => {
-  const seq = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-  let [...q] = seq;
+  const seq = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  let [...q] = seq
   document.addEventListener('keyup', e => {
     if (q[0] === e.keyCode) {
-      q.shift();
+      q.shift()
       if (!q.length) {
         c();
-        [...q] = seq;
+        [...q] = seq
       }
     } else {
-      [...q] = seq;
+      [...q] = seq
     }
-  });
-};
+  })
+}
 
-export default kc;
+export default kc
