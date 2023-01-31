5fe406329018be9d014f0860c94a08e4e69d3cf2
Nicolas LAURENT <nicolas.laurent@lephare.com>
2023-01-31 09:59:34 +0100

    chore(deps): update dependencies

--- dist/konami.js
+++ dist/konami.js
@@ -4,14 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-
 var kc = function kc(c) {
   var seq = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   var q = seq.slice(0);
   document.addEventListener('keyup', function (e) {
     if (q[0] === e.keyCode) {
       q.shift();
-
       if (!q.length) {
         c();
         q = seq.slice(0);
@@ -21,6 +19,5 @@ var kc = function kc(c) {
     }
   });
 };
-
 var _default = kc;
 exports.default = _default;
